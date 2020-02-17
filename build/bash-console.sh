8aec9f4f11e80c0d317796ef8da7f6c033315f82
Jaehyuk Kim <jaehyuk@xsync.co>
2020-02-17 22:29:58 +0900

    BLD: 배포 스크립트 수정

--- build/bash-console.sh
+++ build/bash-console.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
-# ssh -i ./build/bmm-web.pem ubuntu@52.79.120.204 'bash -s' <./build/docker-deploy.sh
 ssh -i ./build/bmm-web.pem ubuntu@52.79.120.204
 
--- build/docker-run.sh
+++ build/docker-run.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -e
-# sudo docker stop $(sudo docker ps -q)
-if [ ! "$(sudo docker ps -q -f name=bmm_app)" ]; then
-    if [ "$(sudo docker ps -aq -f status=exited -f name=bmm_app)" ]; then
-        # cleanup
-        sudo docker rm bmm_app
-        sudo docker load -i bmm_app.tar 
-    fi
-    # run your container
-    sudo docker run -it -p 3000:3000 -d --name bmm_app:latest
-    sudo docker ps
-fi
+# sudo docker stop
+sudo docker stop $(sudo docker ps -qa)
+sudo docker rm $(sudo docker ps -qa) >/dev/null
+# images
+sudo docker rmi $(sudo docker images -qa) >/dev/null
+sudo docker load -i bmm_app.tar
+
+# run your container
+sudo docker run -d -p 3000:3000 bmm_app:latest
+sudo docker ps
 
 # Rollback 을 위해서 우선 주석처리
 # if command -v docker images; then
